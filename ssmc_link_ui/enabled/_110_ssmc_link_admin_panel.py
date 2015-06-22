998b0b800bd744ccd70722eeecc91e18b61e9399
Rich Hagarty <richard.hagarty@hp.com>
2015-06-22 12:32:01 -0700

    no longer need to modify local settings with override

--- ssmc_link_ui/enabled/_110_ssmc_link_admin_panel.py
+++ ssmc_link_ui/enabled/_110_ssmc_link_admin_panel.py
@@ -3,4 +3,7 @@ PANEL_GROUP = 'admin'
 PANEL = 'ssmc_link'
 ADD_PANEL = 'ssmc_link_ui.panel.SSMCLink'
 ADD_INSTALLED_APPS = ['ssmc_link_ui']
+UPDATE_HORIZON_CONFIG = {
+    'customization_module': 'ssmc_link_ui.overrides',
+}
 
