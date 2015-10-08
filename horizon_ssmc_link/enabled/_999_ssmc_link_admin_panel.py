f9fdc71c34b0d4b88389668d204944502a1307f6
Rich Hagarty <richard.hagarty@hp.com>
2015-10-08 16:33:34 -0700

    work with new plug-in structure 3

--- horizon_ssmc_link/enabled/_999_ssmc_link_admin_panel.py
+++ horizon_ssmc_link/enabled/_999_ssmc_link_admin_panel.py
@@ -23,6 +23,7 @@ PANEL_GROUP = 'admin'
 ADD_PANEL = 'horizon_ssmc_link.storage_panel.panel.SSMCLink'
 
 
-#ADD_INSTALLED_APPS = ['horizon_ssmc_link.storage_panel']
-ADD_INSTALLED_APPS = ['horizon_ssmc_link']
+ADD_INSTALLED_APPS = ['horizon_ssmc_link.storage_panel']
+
+UPDATE_HORIZON_CONFIG = { 'customization_module': 'horizon_ssmc_link.overrides', }
 
